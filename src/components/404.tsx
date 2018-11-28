59d7d0b2b306f4ca78e79758e815b753000a39b6
傅峰 <feng.fu@downtown8.com>
2018-11-29 00:13:49 +0800

    11

--- src/components/404.tsx
+++ src/components/404.tsx
@@ -3,7 +3,7 @@ import * as React from 'react'
 //展示型组件最优秀的写法是在参数中使用{xx,xx,xx}配置
 const errors = (props: any) => {
     return (
-        <div>404 not found ！！！</div>
+        <div>404 not found ！！</div>
     )
 }
 
